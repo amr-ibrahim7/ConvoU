1f1a8dec04417e3422c727506257c0e708538f93
amr-ibrahim7 <amrnew10@gmail.com>
2025-10-29 05:37:09 +0300

    feat(security): integrate Arcjet for comprehensive application protection

---
+++ be/src/middleware/arcjet.middleware.ts
@@ -0,0 +1,44 @@
+
+import { NextFunction, Request, Response } from "express";
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+export const arcjetProtection = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const decision = await aj.protect(req);
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res
+          .status(429)
+          .json({ message: "Rate limit exceeded. Please try again later." });
+      }
+      
+      if (decision.reason.isBot()) {
+        return res.status(403).json({ message: "Bot access denied." });
+      }
+
+      return res
+        .status(403)
+        .json({ message: "Access denied by security policy." });
+    }
+
+    if (decision.results.some(isSpoofedBot)) {
+      return res.status(403).json({
+        error: "Spoofed bot detected",
+        message: "Malicious bot activity detected.",
+      });
+    }
+
+    
+    next();
+
+  } catch (error) {
+    console.error("Arcjet protection error:", error);
+    next();
+  }
+};
--- be/src/routes/auth.route.ts
+++ be/src/routes/auth.route.ts
@@ -2,24 +2,25 @@ import express from "express";
 import { signup, login, logout ,updateProfile, getMyProfile, updatePassword } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
 
 
-router.post("/signup",  signup);
+router.post("/signup", arcjetProtection, signup);
 
-router.post("/login", login);
+router.post("/login",arcjetProtection, login);
 
-router.post("/logout", logout);
+router.post("/logout", arcjetProtection,logout);
 
-router.get("/me", protectRoute, getMyProfile);
+router.get("/me", arcjetProtection, protectRoute, getMyProfile);
 
 
 
-router.put("/update", protectRoute, upload.single("profilePic"), updateProfile);
+router.put("/update", arcjetProtection, protectRoute, upload.single("profilePic"), updateProfile);
 
-router.put("/update-password", protectRoute, updatePassword);
+router.put("/update-password", arcjetProtection, protectRoute, updatePassword);
 
 
 
