7c3089b2e9ed099c9ce9cbd3c0be4a2eeb5dfeac
amr-ibrahim7 <amrnew10@gmail.com>
2025-11-03 23:10:34 +0200

    editing generateToken

--- be/src/utils/generateToken.ts
+++ be/src/utils/generateToken.ts
@@ -1,3 +1,24 @@
+// import jwt from "jsonwebtoken";
+// import { Response } from "express";
+
+// const generateToken = (userId: string, res: Response) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET as string, {
+//     expiresIn: "7d",
+//   });
+
+//   res.cookie("jwt", token, {
+//     maxAge: 7 * 24 * 60 * 60 * 1000, //ms
+//     httpOnly: true, // prevent XSS Attacks : cross-site scripting
+//     sameSite: "strict", //CSRF attacks
+//     secure: process.env.NODE_ENV ===  "development" ? false : true, 
+//   });
+
+
+//   return token;
+// };
+
+// export default generateToken;
+
 import jwt from "jsonwebtoken";
 import { Response } from "express";
 
@@ -6,13 +27,14 @@ const generateToken = (userId: string, res: Response) => {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, //ms
-    httpOnly: true, // prevent XSS Attacks : cross-site scripting
-    sameSite: "strict", //CSRF attacks
-    secure: process.env.NODE_ENV ===  "development" ? false : true, 
-  });
+  const isProduction = process.env.NODE_ENV === "production";
 
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    secure: isProduction, // Set to true in production
+    sameSite: isProduction ? "none" : "strict", // Important for cross-domain cookies
+  });
 
   return token;
 };
