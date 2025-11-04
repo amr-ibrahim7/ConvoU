770c03c883332b0c3841a6d33f745ed64946d605
amr-ibrahim7 <amrnew10@gmail.com>
2025-11-04 02:16:56 +0200

    fix: Configure monorepo for Vercel deployment

--- be/src/utils/generateToken.ts
+++ be/src/utils/generateToken.ts
@@ -23,8 +23,6 @@
 
 
 
-
-
 import jwt from "jsonwebtoken";
 import { Response } from "express";
 
@@ -33,13 +31,11 @@ const generateToken = (userId: string, res: Response) => {
     expiresIn: "7d",
   });
 
-  const isProduction = process.env.NODE_ENV === "production";
-
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, //ms
-    httpOnly: true, // prevent XSS Attacks
-    sameSite: isProduction ? "none" : "lax", 
-    secure: isProduction, 
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
   });
 
   return token;
