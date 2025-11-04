68a56ffff9b0fc1a174483b698d7961f0c4b61cf
amr-ibrahim7 <amrnew10@gmail.com>
2025-11-04 06:40:27 +0200

    Update API configuration for rewritesv1

--- be/src/lib/passport.ts
+++ be/src/lib/passport.ts
@@ -7,7 +7,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "/api/auth/google/callback",
+      // callbackURL: "/api/auth/google/callback",
+      callbackURL: process.env.NODE_ENV === "production"
+        ? "https://convo-u-fe.vercel.app/api/auth/google/callback"
+        : "http://localhost:3000/api/auth/callback/google",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
